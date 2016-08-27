0db2ef746181d13bb9a28ed60b3032ffd311a577
Kaloyan83 <cyrens_project@abv.bg>
2016-08-27 18:06:08 +0300

    add changes

--- MyBlog/Models/AccountViewModels.cs
+++ MyBlog/Models/AccountViewModels.cs
@@ -79,6 +79,9 @@ namespace MyBlog.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Display(Name = "User name")]
+        public string FullName { get; set; }
     }
 
     public class ResetPasswordViewModel
--- MyBlog/Models/ApplicationUser.cs
+++ MyBlog/Models/ApplicationUser.cs
@@ -3,13 +3,14 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyBlog.Models
 {
    
     public class ApplicationUser : IdentityUser
     {
-      
+     
         public string FullName { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
